d82220dff31483c6a2a4120ec6e9dd492c033c4c
Sunit Roy <2703iamsry@gmail.com>
2023-06-06 10:39:33 +0530

    compile errors fix

--- src/pages/api/dex.ts
+++ src/pages/api/dex.ts
@@ -3,18 +3,21 @@ import dexterityTs from "@hxronetwork/dexterity-ts";
 const dexterity = dexterityTs;
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 
-const wallet = useWallet();
 
-const CLUSTER_NAME = "devnet";
-const rpc = clusterApiUrl(CLUSTER_NAME);
+export async function Dex(){
+  const wallet = useWallet();
+
+  const CLUSTER_NAME = "devnet";
+  const rpc = clusterApiUrl(CLUSTER_NAME);
 
   // get the latest manifest
   const manifest = await dexterity.getManifest(rpc, false, wallet);
 
-// BTC-USD Market-Product-Group PubKey
-  const MPG = "DDxNzq3A4qKJxnK2PFYeXE1SgGXCR5baaDBhpfLn3LRS"
+  // BTC-USD Market-Product-Group PubKey
+  const MPG = "DDxNzq3A4qKJxnK2PFYeXE1SgGXCR5baaDBhpfLn3LRS";
   const mpgPubkey = new PublicKey(MPG);
 
-  //Create our TRG for the BTC-USD MPG 
+  //Create our TRG for the BTC-USD MPG
   const trgPubkey = await manifest.createTrg(mpgPubkey);
   console.log("success! trg pubkey:", trgPubkey.toBase58());
+}
